0124151d4e1d39f09da42c91f04c54eeed04ee04
Justin Bennett <git@just-be.dev>
2024-09-15 12:55:01 -0500

    Add astro/cloudflare integration (#18)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: cloudflare(),
+  output: "server",
 });
