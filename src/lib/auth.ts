74ff2cec790b4c4408766e34c2afa91c113ba390
Justin Bennett <git@just-be.dev>
2025-05-21 01:22:50 -0400

    WIP: login/signup pages

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,10 @@
 import { db } from "@/db";
-import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
+	basePath: "/auth",
 	database: prismaAdapter(db, {
 		provider: "sqlite", // or "mysql", "postgresql", ...etc
 	}),
