6e810616f36b9f3eac10226248e538550a4e67c5
Justin Bennett <git@just-be.dev>
2025-05-24 20:04:12 +0200

    Update rwsdk; prisma; wrangler

--- src/db.ts
+++ src/db.ts
@@ -1,23 +1,26 @@
+import { PrismaClient } from "@generated/prisma";
 import { PrismaD1 } from "@prisma/adapter-d1";
-import { PrismaClient } from "@prisma/client";
+
+export type * from "@generated/prisma";
 
 export let db: PrismaClient;
 
-export const createDbClient = (env: Env) =>
-	new PrismaClient({ adapter: new PrismaD1(env.DB) });
-
+// context(justinvdm, 21-05-2025): We need to instantiate the client via a
+// function rather that at the module level for several reasons:
+// * For prisma-client-js generator or cases where there are dynamic import to
+//   the prisma wasm modules, we need to make sure we are instantiating the
+//   prisma client later in the flow when the wasm would have been initialized
+// * So that we can encapsulate workarounds, e.g. see `SELECT 1` workaround
+//   below
 export const setupDb = async (env: Env) => {
-	db = createDbClient(env);
-	// todo(justinvdm, 30 Jan 2025): Figure out how to avoid this.
-	//
-	// ## Context:
-	// Vite sends an initial request to the worker when running the dev server,
-	// at which point the Prisma WASM is imported. Using the Prisma for the first time _after_ this initial request
-	// (e.g. if we only run the db for a request to /some/subpath) causes vite to not try import the WASM module
-	// at all, and ultimately the request ends up hanging indefinetely.
-	// * However, once the WASM has been imported, it is cached in some way that persists on the file system
-	// (from experimentation, it is not in node_modules/.vite). This means that if you were to subsequently
-	// change the code to _not_ have Prisma used after the initial request, the WASM will still be cached and
-	// the request will not hang. This makes this issue particularly hard to debug.
+	db = new PrismaClient({
+		// context(justinvdm, 21-05-2025): prisma-client generated type appears to
+		// consider D1 adapter incompatible, though in runtime (dev and production)
+		// it works
+		// @ts-ignore
+		adapter: new PrismaD1(env.DB),
+	});
+
+	// context(justinvdm, 21-05-2025): https://github.com/cloudflare/workers-sdk/pull/8283
 	await db.$queryRaw`SELECT 1`;
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,4 @@
 import { db } from "@/db";
-import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { admin } from "better-auth/plugins";
